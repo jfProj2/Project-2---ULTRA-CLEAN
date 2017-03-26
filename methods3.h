9a3578b9f9c7bbf35ed1df4bd5346e12248e42fd
jfProj2 <jfProj2@gmail.com>
2017-03-26 11:39:13 -0400

    Add files via upload

--- methods3.h
+++ methods3.h
@@ -40,6 +40,7 @@ This is the supporting method file for Project2.
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
+#include <form.h>
 
 #include "methods_main.h"
 
@@ -52,7 +53,7 @@ using namespace std;
 
 int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x);
 int show_error_window(int code, int err_h, int err_w, int err_y, int err_x);
-int show_open_window(int open_h, int open_w, int open_y, int open_x);
+string show_open_window(int open_h, int open_w, int open_y, int open_x);
 int show_save_window(int save_h, int save_w, int save_y, int save_x);
 
 //###################
@@ -225,11 +226,181 @@ int show_error_window(int code, int err_h, int err_w, int err_y, int err_x){
 
 //NEEDS CLEANING
 //NEEDS JAVADOC
+string show_open_window(int open_h, int open_w, int open_y, int open_x){
+  WINDOW *open_win;
+  WINDOW *open_subwin;
+  //forms stuff
+  FORM *form;
+  string filename = "";
+  FIELD *field[2];
+
+  initscr();
+  cbreak();
+  keypad(stdscr, true);
+ 
+
+  field[0] = new_field(1, open_w/2, 0, 0, 0, 0);
+  field[1] = NULL;
+  set_field_back(field[0], A_UNDERLINE);
+
+  form = new_form(field);
+  //scale?
+  
+  open_win = newwin(open_h, open_w, open_y, open_x);
+  open_subwin = derwin(open_win, (open_h/2), (open_w/2), (open_h*.4), (open_w/4));
+  
+  keypad(open_win, true);
+  
+
+  set_form_win(form, open_win);
+  set_form_sub(form, open_subwin);
+  box(open_win, 0, 0);
+  // box(form, 0, 0);
+  post_form(form);
+  mvwprintw(open_win, 1, open_w/2 -9, "Enter File to Open");
+  mvwprintw(open_win, 2, open_w/2 -7, "And Press Enter");
+  
+  wrefresh(open_win);
+  refresh();
+  
+  int ch;
+  bool selected = false;
+  while((ch = wgetch(open_win)) != KEY_F(1) && !selected){
+    if(ch >= 32 && ch < 127){
+      form_driver(form, ch);
+      filename += ch;
+    }
+    else if(ch == 10){
+      selected = true;
+    }
+  }
+
+  // wrefresh(open_win);
+  unpost_form(form);
+  free_form(form);
+  free_field(field[0]);
+  free_field(field[1]);
+  delwin(open_win);
+  delwin(open_subwin);
+  //endwin();
+  wborder(open_win,' ',' ',' ',' ',' ',' ',' ',' ');
+  clear();
+  return filename; //USER EXITS MENU (F1 Pressed)
+}
+
+//NEEDS CLEANING
+//NEEDS JAVADOC
+
+int show_save_window(int save_h, int save_w, int save_y, int save_x){
+  WINDOW *save_win;
+  WINDOW *save_subwin;
+  MENU *save_menu;
+  ITEM **items;
+  initscr();
+  const char * choices[] = {"Yes", "No", "Cancel"};
+  int n_choices = ARRAY_SIZE(choices);
+  items = (ITEM**)calloc(n_choices+1, sizeof(ITEM*));
+  for(int i = 0; i < n_choices; i++){
+    items[i] = new_item(choices[i], NULL);
+  }
+  items[n_choices] = (ITEM*)NULL;
+  save_menu = new_menu((ITEM**)items);
+  save_win = newwin(save_h, save_w, save_y, save_x);
+  mvwprintw(save_win, 1, (save_w/2 - 9),"Do you wish to save?");
+  save_subwin = derwin(save_win, (save_h/2), (save_w/2), (save_h*0.4), (save_w/3));
+  keypad(save_win, true);
+
+  set_menu_win(save_menu, save_win);
+  set_menu_sub(save_menu, save_subwin);
+  set_menu_mark(save_menu, "* ");
+  
+  // mvwprintw(save_win, 1, open_w/2 -9, "Enter File to Open");
+  box(save_win, 0, 0);
+  wrefresh(save_win);
+  refresh();
+  cbreak();
+  post_menu(save_menu);
+  wrefresh(save_win);
+
+  int key = 1;
+  int choice = 0;
+  bool selected = false;
+
+  while(!selected){
+    key = wgetch(save_win);
+    switch(key){
+    case KEY_DOWN:
+      menu_driver(save_menu, REQ_DOWN_ITEM);
+      break;
+    case KEY_UP:
+      menu_driver(save_menu, REQ_UP_ITEM);
+      break;
+    case 10:
+      //MORE TO DO HERE?
+      selected = true;
+      break;
+    }
+    wrefresh(save_win);
+  }
+
+  if(selected){
+    ITEM *cur_item = current_item(save_menu);
+    const char * it = item_name((const ITEM*)cur_item);
+    if(strcmp(it,"No") == 0){
+      choice = 0;
+    } else if (strcmp(it,"Yes") == 0){
+      choice = 1;
+    } else if (strcmp(it,"Cancel") == 0){
+      choice = 2;
+    }
+  }
+
+  unpost_menu(save_menu);
+  for(int i=0; i < n_choices; i++){
+    free_item(items[i]);
+  }
+
+  wborder(save_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+  mvwprintw(save_win, save_y+10, save_x+10,"");
+  wrefresh(save_win);
+  //mvprintw(2, (menu_w/2)-4, NULL);
+  wrefresh(save_win);
+  free_menu(save_menu);
+  wrefresh(save_win);
+  wclear(save_win);
+  wclear(save_subwin);
+  refresh();
+  delwin(save_subwin);
+  delwin(save_win);
+  // endwin();
+  wclear();
+  return choice; //USER EXITS MENU (F1 Pressed)
+}
+
+
+#endif
+
+
+
+/**OLD OPEN
+//NEEDS CLEANING
+//NEEDS JAVADOC
 int show_open_window(int open_h, int open_w, int open_y, int open_x){
   WINDOW *open_win;
   WINDOW *open_subwin;
   MENU *open_menu;
   ITEM **items;
+
+  //forms stuff
+  FORM *form;
+  FIELD *field[1];
+  field[0] = new_field(1, open_w/2, 0, 0, 0, 0);
+  form = new_form(field);
+  post_form(form);
+  refresh();
+  
+  //</> forms stuff
+  
   initscr();
   const char * choices[] = {"Open"};
   int n_choices = ARRAY_SIZE(choices);
@@ -284,10 +455,13 @@ int show_open_window(int open_h, int open_w, int open_y, int open_x){
     free_item(items[i]);
   }
 
-  wborder(open_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+  // wborder(open_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
   wrefresh(open_win);
   //mvprintw(2, (menu_w/2)-4, NULL);
   wrefresh(open_win);
+  unpost_form(form);
+  free_form(form);
+  free_field(field[0]);
   free_menu(open_menu);
   wrefresh(open_win);
   delwin(open_subwin);
@@ -298,84 +472,4 @@ int show_open_window(int open_h, int open_w, int open_y, int open_x){
 
 //NEEDS CLEANING
 //NEEDS JAVADOC
-int show_save_window(int save_h, int save_w, int save_y, int save_x){
-  WINDOW *save_win;
-  WINDOW *save_subwin;
-  MENU *save_menu;
-  ITEM **items;
-  initscr();
-  const char * choices[] = {"Yes", "No", "Cancel"};
-  int n_choices = ARRAY_SIZE(choices);
-  items = (ITEM**)calloc(n_choices+1, sizeof(ITEM*));
-  for(int i = 0; i < n_choices; i++){
-    items[i] = new_item(choices[i], NULL);
-  }
-  items[n_choices] = (ITEM*)NULL;
-  save_menu = new_menu((ITEM**)items);
-  save_win = newwin(save_h, save_w, save_y, save_x);
-  save_subwin = derwin(save_win, (save_h/2), (save_w/2), (save_h*0.4), (save_w/3));
-  keypad(save_win, true);
-
-  set_menu_win(save_menu, save_win);
-  set_menu_sub(save_menu, save_subwin);
-  set_menu_mark(save_menu, "* ");
-  mvwprintw(save_win, 0, 0,"Do you wish to save the current text?");
-  box(save_win, 0, 0);
-  wrefresh(save_win);
-  cbreak();
-  post_menu(save_menu);
-  wrefresh(save_win);
-
-  int key = 1;
-  int choice = 0;
-  bool selected = false;
-
-  while(!selected){
-    key = wgetch(save_win);
-    switch(key){
-    case KEY_DOWN:
-      menu_driver(save_menu, REQ_DOWN_ITEM);
-      break;
-    case KEY_UP:
-      menu_driver(save_menu, REQ_UP_ITEM);
-      break;
-    case 10:
-      //MORE TO DO HERE?
-      selected = true;
-      break;
-    }
-    wrefresh(save_win);
-  }
-
-  if(selected){
-    ITEM *cur_item = current_item(save_menu);
-    const char * it = item_name((const ITEM*)cur_item);
-    if(strcmp(it,"No") == 0){
-      choice = 0;
-    } else if (strcmp(it,"Yes") == 0){
-      choice = 1;
-    } else if (strcmp(it,"Cancel") == 0){
-      choice = 2;
-    }
-  }
-
-  unpost_menu(save_menu);
-  for(int i=0; i < n_choices; i++){
-    free_item(items[i]);
-  }
-
-  wborder(save_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-  mvwprintw(save_win, save_y+10, save_x+10,"");
-  wrefresh(save_win);
-  //mvprintw(2, (menu_w/2)-4, NULL);
-  wrefresh(save_win);
-  free_menu(save_menu);
-  wrefresh(save_win);
-  delwin(save_subwin);
-  delwin(save_win);
-  // endwin();
-  return choice; //USER EXITS MENU (F1 Pressed)
-}
-
-
-#endif
+*/
